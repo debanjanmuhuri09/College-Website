7e0d77c855e0d1f6a2179c6c60a306500c01584d
RedWiz09 <muhuridebanjan@gmail.com>
2024-04-27 18:28:09 +0530

    responsive-navbar

---
+++ script.js
@@ -0,0 +1,11 @@
+
+    function showSideBar(){
+            const sidebar= document.querySelector('.side-bar')
+            sidebar.style.display='flex'
+            const navbar=document.querySelector('.nav-bar')
+            navbar.style.display='none'
+    }
+    function hideSidebar(){
+            const sidebar= document.querySelector('.side-bar')
+            sidebar.style.display='none'
+    } 
